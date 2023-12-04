d672ca7049631b2f97492e0eedaed10e3a86f4d8
Agustinlorenzo09 <agustinlorenzo011@outlook.es>
2023-12-04 20:18:26 -0300

    commit 2 cambio navbar

--- src/assets/componentes/Navbar/Navbar.jsx
+++ src/assets/componentes/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import classes from'./navbar.module.css'
+import { catalogo } from "../../../prodcutosMock";
 const Navbar= ()=>{
     return(
         <div>
             <div className= {classes.container}>
-             <img src="https://scontent.cdninstagram.com/v/t51.2885-19/350860415_804549984624090_9113172392647762730_n.jpg?stp=dst-jpg_s80x80&_nc_cat=108&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=PQL63Tf2r9sAX9AtTtd&_nc_ht=scontent.cdninstagram.com&oh=00_AfD382QcNKliBsUwbm657eL0qMELzvpNRLWvWiIr2SV-Lw&oe=656C10E2" alt=""  className={classes.logo}/>
+             <img src="./../imagenes/lamartinalogo.jpg" alt="" srcset="" />
             <div>
             <nav className={classes.navContainer}>
             <a href="http://"className={classes.navItem}>living</a>
